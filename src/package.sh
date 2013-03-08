a804ab2caf668d408e3adccd4b946213ca821a91
Daniel Uber <djuber@gmail.com>
2013-03-08 14:39:24 -0600

    updated package.sh to omit .git directory from zipfile

--- src/package.sh
+++ src/package.sh
@@ -6,7 +6,8 @@ make clean
 
 # create zip archive of current directory
 cd ../..
-rm -f CS408-Project1-Uber.zip;
-zip -r CS408-Project1-Uber.zip divide-and-conquer-sat
+rm -f CS408-Project1-Uber.zip
+zip -r CS408-Project1-Uber.zip divide-and-conquer-sat -i dividie-and-conquer-sat/src divide-and-conquer-sat/README  -i divide-and-conquer-sat/src/* -i divide-and-conquer-sat/cnf.exe
+
 
 rm -f divide-and-conquer-sat/cnf.exe
