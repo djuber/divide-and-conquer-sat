9448146de3602ff624d233a86c5d37169118ccfc
Daniel Uber <djuber@gmail.com>
2013-03-08 13:18:23 -0600

    cleaned up test.sh to redirect time output to file and to only check small cases in benchmarks.

--- src/test.sh
+++ src/test.sh
@@ -3,14 +3,14 @@
 date > test-results
 echo >> test-results
 # run tests, record output
-for i in  ../U*/*.cnf  
+for i in  ../U*20*/*.cnf  ../U*50-218*/*.cnf
 do
     echo $i >> test-results
     # this interleaves output from cnf program, and timing information
     # time takes an optional format string which could be used to clean
     # this up.
     # this line works on linux, not mac, apples time is not as clever
-    time  ./cnf $i  >> test-results
+    time  -o test-results -a ./cnf $i  >> test-results
     echo >> test-results
     echo >> test-results
 done
